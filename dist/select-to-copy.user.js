d7350a7b7d06756900c2538489b7b3482c0b60ea
wChenonly <wChenonly@qq.com>
2023-06-30 15:32:46 +0800

    init

---
+++ dist/select-to-copy.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         select-to-copy
+// @namespace    https://github.com/wChenonly/select-to-copy
+// @version      0.0.1
+// @author       wChenonly
+// @description  Select to copy and use in the browser.
+// @include      *://**/*
+// @grant        unsafeWindow
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  let text;
+  document.onclick = function() {
+    if (unsafeWindow.getSelection) {
+      text = unsafeWindow.getSelection();
+    } else if (document.selection) {
+      text = document.selection.createRange();
+    }
+    const v = text == null ? void 0 : text.toString();
+    if (v) {
+      navigator.clipboard.writeText(v);
+    }
+  };
+
+})();
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import monkey from "vite-plugin-monkey";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    monkey({
+      entry: "src/main.ts",
+      userscript: {
+        namespace: "https://github.com/wChenonly/select-to-copy",
+        include: "*://**/*",
+        grant: "unsafeWindow",
+      },
+    }),
+  ],
+});
